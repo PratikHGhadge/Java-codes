9ff7ca28abca85458fee28665bf43a41fab47cf8
Pratik <pratikhg.2001@gmail.com>
2022-05-21 11:51:47 +0530

    Stack is modified

--- src/Stack.java
+++ src/Stack.java
@@ -1,32 +1,56 @@
-interface StackMethods{
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+interface StackMethods {
     void push(int val);
+
     int pop();
+
     int size();
+
     boolean isEmpty();
+
     void display();
 }
+
 public class Stack implements StackMethods {
     int top;
     int[] arr;
-    Stack(){
-        top=-1;
+
+    Stack() {
+        top = -1;
         arr = new int[20];
     }
+
     @Override
-    public void display(){
-        for (int i = 0; i <= top; i++) {
-            System.out.print(arr[i]+" ");
+    public void display() {
+        if (top == -1) {
+            System.out.println("STACK IS EMPTY...");
+            return;
         }
+        System.out.println("Stack is :");
+        for (int i = 0; i <= top; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 
     @Override
     public void push(int val) {
+        if (top == arr.length - 1) {
+            System.out.println("STACK IS FULL...");
+            return;
+        }
         top++;
         arr[top] = val;
     }
 
     @Override
     public int pop() {
+        if (top == -1) {
+            System.out.println("STACK IS EMPTY...");
+            return -1;
+        }
         int x = arr[top];
         top--;
         return x;
@@ -39,17 +63,53 @@ public class Stack implements StackMethods {
 
     @Override
     public boolean isEmpty() {
-        return top==-1;
+        return top == -1;
     }
+
+
     public static void main(String[] args) {
+        int ans, ch;
         Stack obj = new Stack();
-        System.out.println(obj.isEmpty());
-        obj.push(7);
-        obj.push(3);
-        obj.push(2);
-        obj.push(9);
-        System.out.println(obj.isEmpty());
-        System.out.println(obj.pop());
-        obj.display();
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("******************* Stack **********************");
+            System.out.println("1 Push ");
+            System.out.println("2 Pop ");
+            System.out.println("3 isEmpty ");
+            System.out.println("4 Display ");
+            System.out.println("Enter your choice :");
+            ch = sc.nextInt();
+            switch (ch) {
+                case 1: {
+                    System.out.println("Enter int to push :");
+                    int temp = sc.nextInt();
+                    obj.push(temp);
+                    break;
+                }
+
+                case 2: {
+                    System.out.println("Deleted int = " + obj.pop());
+                    break;
+                }
+
+                case 3: {
+                    if (obj.isEmpty()) {
+                        System.out.println("Stack is Empty");
+                    } else {
+                        System.out.println("Stack is not empty");
+                    }
+                    break;
+                }
+                case 4: {
+                    obj.display();
+                    break;
+                }
+                default:
+                    System.out.println("enter correct choice");
+                    break;
+            }
+            System.out.println("Do you want to continue :");
+            ans = sc.nextInt();
+        } while (ans == 1);
     }
 }
